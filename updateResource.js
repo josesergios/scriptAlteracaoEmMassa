8569d35b4cb1daa2495d7d6c0629558926ab5a69
Sérgio Santos <josesergio.s@hotmail.com>
2023-06-16 16:33:52 -0300

    updateVariableOfAllRouter

--- updateResource.js
+++ updateResource.js
@@ -13,30 +13,34 @@ const listVariablesResource = require('./listVariablesResource')
 
 try {
     /*LISTAR TODAS AS VARIÁVEIS DO RESOURCE*/
-    let variables = async () => {
+    async function variables(){
         return listVariablesResource.variablesResource();
     }
     // variables().then(data=>console.log(data));
 
     /*LISTAR TODOS OS ROUTERS DE ISP VENDAS*/
-    let routers = async () => {
+    async function routers(){
         return listRouters.listRouter()
     }
 
-    /*Pegar o ID e NOME de todos os routes de ISP Vendas*/
+    /*PEGAR O ID E NOME DE TODOS OS ROUTERS DO PROJETO ISP VENDAS*/
     async function listRoutersByNameAndId() {
-        routers().then(data=>{
-            let listRouter = data
-            let cont = 0
-            for (let value of listRouter) {
-                console.log(`Router name: ${value.name}\nRouter ID: ${value._id}`);
-                console.log("");
-                cont++;
+
+        let refactorList = await routers().then(data=>{
+            let list = data
+            let newData = [];
+            for (let value of list) {
+                newData.push({
+                    id: value._id,
+                    name: value.name
+                })
               }
-              console.log(`Total de ISPs: ${cont}`);
+              return newData
         });
+        //console.log(refactorList);
+        return refactorList
     }
-listRoutersByNameAndId()
+    //listRoutersByNameAndId()
     async function updateVariableByRouter(id) {
         routerApi.patch(`/router/settings/add-variables/${id}`, {
             variables: {
@@ -53,14 +57,42 @@ listRoutersByNameAndId()
       //updateVariableByRouter("644aa97731be6731634da3ff");
 
     async function updateVariableOfAllRouter(){
-        for (let value of listRouter) {
-           console.log(value.name);
-         }
+        let data = await listRoutersByNameAndId()
+        for (let value of data) {
+        routerApi.patch(`/router/settings/add-variables/${value.id}`, {
+            variables: {
+                blockedPhoneNumbers: "[\"553799999998\",\"5513974255154\",\"5513991688975\",\"5513996230639\",\"5513991552083\",\"5513981442153\"]"
+            },
+            syncBlip: true
+            }
+        ).then(response => {
+            console.log(response.status);
+            console.log("Atualização bem sucedida");
+            console.log("Novo valor da variável: ", response.data.data.variables.blockedPhoneNumbers);
+        })
     }
-
- } catch (error) {
-   console.log(error)
- }
+    /*
+        //USAR PARA FINS DE TESTES
+        data = [{id: "644aa97731be6731634da3ff", name: "test"}]
+        for (let value of data) {
+            routerApi.patch(`/router/settings/add-variables/${value.id}`, {
+                variables: {
+                    blockedPhoneNumbers: "[\"553799999998\",\"5513974255154\",\"5513991688975\",\"5513996230639\",\"5513991552083\",\"5513981442153\"]"
+                },
+                syncBlip: false
+                }
+            ).then(response => {
+                console.log(response.status);
+                console.log("Atualização bem sucedida");
+                console.log("Novo valor da variável: ", response.data.data.variables.blockedPhoneNumbers);
+            })
+            }
+        */
+      }
+      //updateVariableOfAllRouter();
+    } catch (error) {
+    console.log(error)
+    }
 
 
 
